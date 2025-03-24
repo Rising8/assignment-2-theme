03032bd794a8514b84db4ab51ccdc721e07a081b
Edmand <20947873@student.curtin.edu.au>
2025-03-24 16:36:03 +0800

    Enqueueing BootStrap

--- functions.php
+++ functions.php
@@ -10,6 +10,12 @@ add_action('after_setup_theme', 'esports_theme_setup');
 function esports_enqueue_styles() 
 {
     wp_enqueue_style('esports-style', get_stylesheet_uri());
+
+    // Enqueue Bootstrap CSS
+    wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+
+    // Enqueue Bootstrap JS
+    wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'esports_enqueue_styles');
 
